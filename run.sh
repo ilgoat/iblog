9d72c90ced1554684a6b4569551f9c0f5ac278a0
edenzhong <toyzhong@gmail.com>
2018-11-10 16:13:04 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nohup hugo --config=posts.toml,cards.toml server &
+nohup hugo --config=posts.toml,cards.toml server -d `pwd`/www --baseURL="http://blog.gophergo.cn" --bind="0.0.0.0" &
